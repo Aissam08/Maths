b060c6a4d375ff201bdb6c551c61f0441e1c6b67
Aissam08 <rabhi.aissam@hotmail.fr>
2020-04-05 12:18:58 +0200

    first2

--- pysrc/tools/cnds_initiales.py
+++ pysrc/tools/cnds_initiales.py
@@ -40,26 +40,3 @@ class Initials :
 
 	def addCond(self,co,st):
 		self._ini.append(Initial(co,st.style()))
-
-##On crée une liste d'instance de Initial vide.
-#On crée une instance de Initials contenant cette liste, on y ajoute les éléments I
-#Le style est défini ave 'b-' de base
-#ini.Initial((0,0),{'b':'-'})]
-"""
-I = []
-L1 = [1,2,3]
-L2 = [5,4]
-Init = Initials(I)
-Init.genCond(L1,L2,blue_solid)
-op = (1,0)
-
-for a in Init.getIni():
-	print("Coord : "+str(a.getCoord())+" style : "+str(a.getParam()))
-
-Init.addCond(op,black_dash)
-Init.addCond((4,3),red_dot)
-Init.addCond((2,1),yellow_solid)
-print("\nCoord après ajout\n")
-for a in Init.getIni():
-	print("Coord : "+str(a.getCoord())+" style : "+str(a.getParam()))
-"""
